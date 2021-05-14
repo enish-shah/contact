54cfcd1f7df25a554bc68effb66583dc80654aa9
enish-shah <enishshah1@gmail.com>
2021-05-14 23:10:36 +0000

    first commit

---
+++ contact.js
@@ -0,0 +1,37 @@
+
+  /**
+   * Mobile nav dropdowns activate
+   */
+   on('click', '.navbar .dropdown > a', function(e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+  }, true)
+
+
+  
+  /**
+   * Animation on scroll
+   */
+   window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    })
+  });
+
+
+
+   /**
+   * Mobile nav dropdowns activate
+   */
+    on('click', '.navbar .dropdown > a', function(e) {
+      if (select('#navbar').classList.contains('navbar-mobile')) {
+        e.preventDefault()
+        this.nextElementSibling.classList.toggle('dropdown-active')
+      }
+    }, true)
+  
